e776c0706e0e098b63d82af101a92abd68bfdd37
q522749907 <522749907@qq.com>
2020-04-26 13:53:09 +0800

    tabber

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
-
+const Home = () =>
+  import ('../views/home/Home')
+const Category = () =>
+  import ('../views/category/Category')
+const Cart = () =>
+  import ('../views/cart/Cart')
+const Profile = () =>
+  import ('../views/profile/Profile')
+const Detail = () =>
+  import ('../views/detail/Detail')
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+  {
+    // 动态路由
+    path: '/detail/:iid',
+    component: Detail
   }
 ]
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack:{
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        '@': 'src',
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network',
+        /*'router':'@/router',
+        'store':'@/store',*/
+        'views':'@/views',
+      }
+    },
+  }
 }
